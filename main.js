56e62056096fea74404f3375b67fadf4345e2eac
jack-kovensky <kovensky.j@northeastern.edu>
2023-02-15 22:05:12 -0500

    jk 1

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-console.log("linked")
 
-// Set the dimensions and margins of the graph
+console.log("linked");
+
+
 var margin = { top: 20, right: 30, bottom: 50, left: 50 },
   width = 400 - margin.left - margin.right,
   height = 400 - margin.top - margin.bottom;
@@ -14,36 +15,36 @@ var svg = d3
   .append("g")
   .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Parse the Data
 var data = [55000, 48000, 27000, 66000, 90000];
 
-// X axis
-var x = d3.scaleBand().range([0, width]).padding(0.1);
-x.domain([""]);
+// Add X axis
+var x = d3
+  .scaleBand()
+  .range([0, width])
+  .padding(0.1)
+  .domain(data.map(function(d, i) { return i; }));
 
 svg
   .append("g")
   .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
+  .call(d3.axisBottom(x))
+  .selectAll("text")
+    .style("text-anchor", "end");
 
 // Add Y axis
 var y = d3.scaleLinear().domain([0, 100000]).range([height, 0]);
 svg.append("g").call(d3.axisLeft(y));
 
-// Bars
-svg
-  .selectAll("mybar")
+// Add dots
+svg.selectAll("mydots")
   .data(data)
   .enter()
-  .append("rect")
-  .attr("x", function (d) {
-    return x("");
-  })
-  .attr("y", function (d) {
-    return y(d);
-  })
-  .attr("width", x.bandwidth())
-  .attr("height", function (d) {
-    return height - y(d);
-  })
-  .attr("fill", "#69b3a2");
+  .append("circle")
+  .attr("cx", function(d, i) { return x(i); })
+  .attr("cy", function(d) { return y(d); })
+  .attr("r", 5)
+  .style("fill", "red");
+
+
+
+
